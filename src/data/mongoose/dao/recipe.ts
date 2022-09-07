46bd6d53a31a6eea21207ede02d8bcecabd1cdec
Constantine Theocharis <cthe@mailbox.org>
2022-09-07 11:58:58 +0000

    Use classes instead of lambda blocks

--- src/data/mongoose/dao/recipe.ts
+++ src/data/mongoose/dao/recipe.ts
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
-import { Recipe, RecipeWithoutIngredients } from "../../../types/recipe";
+import { PaginatedResult, Pagination } from "../../../types/pagination";
+import {
+  CreateRecipe,
+  Recipe,
+  RecipeWithoutIngredients,
+  UpdateRecipe,
+} from "../../../types/recipe";
+import { RecipeSearchQuery } from "../../../types/recipe-search-query";
 import { assertConforms } from "../../../utils/assertions";
 import { RecipeDao } from "../../dao/recipe";
 import { getRecipeModel, RecipeClass } from "../schema/recipe";
@@ -26,64 +33,73 @@ const mongoRecipeToRecipeDto = (mongooseRecipe: RecipeClass): Recipe =>
     })),
   });
 
-export const makeMongooseRecipeDao = (
-  connection: mongoose.Connection
-): RecipeDao => {
-  const RecipeModel = getRecipeModel(connection);
+export class RecipeDaoMongoose implements RecipeDao {
+  constructor(
+    connection: mongoose.Connection,
+    private readonly RecipeModel = getRecipeModel(connection)
+  ) {}
 
-  return {
-    create: async (recipe) => {
-      const createdRecipe = await RecipeModel.create({
-        name: recipe.name,
-        description: recipe.description,
-        tags: recipe.tags,
-        ingredients: recipe.ingredients.map((quantifiedIngredient) => ({
-          ingredientId: quantifiedIngredient.ingredientId,
-          quantity: quantifiedIngredient.quantity,
-        })),
-      });
-      return createdRecipe._id.toString();
-    },
-    search: async (query, pagination) => {
-      const mongoQuery = {
-        ...(query.text && { $text: { $search: query.text } }),
-        tags: { $in: query.tags },
-      };
-
-      const items = await RecipeModel.find(mongoQuery)
-        .skip(pagination.skip)
-        .limit(pagination.take)
-        .lean();
-      const total = await RecipeModel.countDocuments(mongoQuery);
-
-      return {
-        items: items.map(mongoRecipeToRecipeDto),
-        total,
-      };
-    },
-    findById: async (id) => {
-      const result = await RecipeModel.findById(id).lean();
-      if (result !== null) {
-        return mongoRecipeToRecipeDto(result);
-      } else {
-        return null;
-      }
-    },
-    findByIdWithoutIngredients: async (id) => {
-      const result = await RecipeModel.findById(id, {
-        ingredients: false,
-      }).lean();
-      if (result !== null) {
-        return mongoRecipeToRecipeDtoWithoutIngredients(result);
-      } else {
-        return null;
-      }
-    },
-    update: async (id, recipe) => {
-      await RecipeModel.findByIdAndUpdate(id, recipe);
-    },
-    delete: async (id) => {
-      await RecipeModel.findByIdAndDelete(id);
-    },
+  create = async (recipe: CreateRecipe): Promise<string> => {
+    const createdRecipe = await this.RecipeModel.create({
+      name: recipe.name,
+      description: recipe.description,
+      tags: recipe.tags,
+      ingredients: recipe.ingredients.map((quantifiedIngredient) => ({
+        ingredientId: quantifiedIngredient.ingredientId,
+        quantity: quantifiedIngredient.quantity,
+      })),
+    });
+    return createdRecipe._id.toString();
   };
-};
+
+  search = async (
+    query: RecipeSearchQuery,
+    pagination: Pagination
+  ): Promise<PaginatedResult<Recipe>> => {
+    const mongoQuery = {
+      ...(query.text && { $text: { $search: query.text } }),
+      tags: { $in: query.tags },
+    };
+
+    const items = await this.RecipeModel.find(mongoQuery)
+      .skip(pagination.skip)
+      .limit(pagination.take)
+      .lean();
+    const total = await this.RecipeModel.countDocuments(mongoQuery);
+
+    return {
+      items: items.map(mongoRecipeToRecipeDto),
+      total,
+    };
+  };
+
+  findById = async (id: string): Promise<Recipe | null> => {
+    const result = await this.RecipeModel.findById(id).lean();
+    if (result !== null) {
+      return mongoRecipeToRecipeDto(result);
+    } else {
+      return null;
+    }
+  };
+
+  findByIdWithoutIngredients = async (
+    id: string
+  ): Promise<RecipeWithoutIngredients | null> => {
+    const result = await this.RecipeModel.findById(id, {
+      ingredients: false,
+    }).lean();
+    if (result !== null) {
+      return mongoRecipeToRecipeDtoWithoutIngredients(result);
+    } else {
+      return null;
+    }
+  };
+
+  update = async (id: string, recipe: UpdateRecipe): Promise<void> => {
+    await this.RecipeModel.findByIdAndUpdate(id, recipe);
+  };
+
+  delete = async (id: string): Promise<void> => {
+    await this.RecipeModel.findByIdAndDelete(id);
+  };
+}
