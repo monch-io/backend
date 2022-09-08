e17f19a1784ffaaaa7c5b8aadcb04e5b20735b5e
Constantine Theocharis <cthe@mailbox.org>
2022-09-08 16:59:10 +0000

    Add TRPC playground and source maps

--- src/service/init.ts
+++ src/service/init.ts
@@ -4,18 +4,34 @@ import { GlobalContext } from "./context";
 import { appRouter } from "./router";
 import { CONFIG } from "../utils/config";
 import { LOG } from "../utils/log";
+import * as trpcPlayground from "trpc-playground/handlers/express";
 
-const app = express();
+const API_ENDPOINT = "/api";
+const API_PLAYGROUND_ENDPOINT = "/api-playground";
 
 // Start the API server
 export const startService = async (
   globalContext: GlobalContext
 ): Promise<void> => {
+  const app = express();
+
   app.use(
-    "/api",
+    API_ENDPOINT,
     trpcExpress.createExpressMiddleware({
       router: appRouter,
       createContext: () => ({ ...globalContext }),
+      onError: ({ error }) => {
+        LOG.error(error);
+      },
+    })
+  );
+
+  app.use(
+    API_PLAYGROUND_ENDPOINT,
+    await trpcPlayground.expressHandler({
+      trpcApiEndpoint: API_ENDPOINT,
+      playgroundEndpoint: API_PLAYGROUND_ENDPOINT,
+      router: appRouter,
     })
   );
 
