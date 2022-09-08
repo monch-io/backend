bbfbb70c89625f6c47fa6f4b300198942506ca62
Constantine Theocharis <cthe@mailbox.org>
2022-09-08 17:54:19 +0000

    Fix quantity type in mongoose

--- src/data/mongoose/schema/quantified-ingredient.ts
+++ src/data/mongoose/schema/quantified-ingredient.ts
@@ -1,12 +1,12 @@
 import { prop, Ref } from "@typegoose/typegoose";
 import mongoose from "mongoose";
-import { Quantity } from "../../../types/quantity";
 import { IngredientClass } from "./ingredient";
+import { QuantityClass } from "./quantity";
 
 export class QuantifiedIngredientClass {
   @prop({ required: true, ref: () => IngredientClass })
   ingredientId!: Ref<IngredientClass, mongoose.Types.ObjectId>;
 
   @prop({ required: true })
-  quantity!: Quantity;
+  quantity!: QuantityClass;
 }
